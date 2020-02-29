af0ebcefc9ab93fb26d0bfd1c6b98046370925d3
hkbenchan <benchanhp@gmail.com>
2020-02-29 15:37:10 +0800

    Updates

---
+++ precache-manifest.4f6111839bbe89585fd6feec3a9d11d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a038e8b3461174b742cb48e54221f82d",
+    "url": "/nonogram-news/index.html"
+  },
+  {
+    "revision": "18266e0c511b5f2a3e1d",
+    "url": "/nonogram-news/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4cc989edd46b4f013b77",
+    "url": "/nonogram-news/static/js/2.3c51843b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nonogram-news/static/js/2.3c51843b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18266e0c511b5f2a3e1d",
+    "url": "/nonogram-news/static/js/main.a0ffe828.chunk.js"
+  },
+  {
+    "revision": "6209989f3366377b6a9d",
+    "url": "/nonogram-news/static/js/runtime-main.e18efb2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nonogram-news/static/media/logo.5d5d9eef.svg"
+  }
+]);
